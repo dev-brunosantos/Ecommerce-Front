813927b489cf296316f43d7dcf5a3f0145d862ce
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-04-15 19:28:53 -0300

    Criando estilos para o componente

--- src/components/Cards/Produto.tsx
+++ src/components/Cards/Produto.tsx
@@ -10,21 +10,23 @@ interface ProdutoInfor {
 
 export const CardProduto = ({ id, nome, descricao, imagem, preco }:ProdutoInfor) => {
     return(
-        <div>
+        <div className="w-[250px] h-[320px] mt-2 mb-2 border-2 border-white rounded-2xl flex flex-col items-center justify-evenly gap-2 bg-white">
             <div>
                 <Image 
-                    width={100}
+                    width={120}
                     height={100}
                     alt="Imagem do Produto"
                     src={imagem}
+
+                    onClick={() => alert(descricao)}
                 />
             </div>
 
-            <div>
-                <h1>{nome}</h1>
-                <p>{descricao}</p>
+            <div className="flex flex-col items-center justify-center gap-2 text-center text-gray-900">
+                <h1 className="font-bold">{nome}</h1>
+                {/* <p>{descricao}</p> */}
                 <span>R$ {preco}</span>
-                <button onClick={() => alert({id})}>Adicionar ao Carrinho</button>
+                <button onClick={() => alert(id)}>Adicionar ao Carrinho</button>
             </div>
         </div>
     )
