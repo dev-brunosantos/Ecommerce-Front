2a0936467b1ff16cc77397a6ec8a10c87933844a
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-04-15 19:35:00 -0300

    Criando estilos para o menu principal

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export const Menu = () => {
     return(
-        <header>
-            <nav>
+        <header className="w-full h-[70px]  bg-gray-900 text-white">
+            <nav className="w-full max-w-[1500px] h-full m-auto flex items-center justify-between px-10 border-4">
                 <div>
                     <Link href='/pages/home'>Loja</Link>
                 </div>
 
-                <ul>
+                <ul className="w-[30%] h-full flex items-center justify-evenly text-center">
                     <li><Link href='/pages/home'>Home</Link></li>
                     <li><Link href='/pages/carrinho'>Carrinho</Link></li>
                     <li><Link href='/pages/contato'>Contato</Link></li>
