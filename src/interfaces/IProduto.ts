508cf842371009417952222a6d3bc4dbe2e2f860
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-04-16 10:37:54 -0300

    Criando a interface de produto

---
+++ src/interfaces/IProduto.ts
@@ -0,0 +1,15 @@
+export interface ProdutoInfor {
+    id: number,
+    nome: string,
+    descricao: string,
+    preco: number,
+    imagem: string
+}
+
+export interface ProdutoApi {
+    id: number,
+    title: string,
+    description: string,
+    price: number,
+    image: string
+}
