1f1d2171814a86cca45e98ebb17170c7fcd27364
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-04-16 16:51:16 -0300

    Corrigindo o posicionamento do botão de carrinho

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,20 +2,20 @@ import Link from "next/link"
 import { BtnCarrinho } from "./Btn/btnCarrinho"
 
 export const Menu = () => {
-    return(
+    return (
         <header className="w-full h-[70px]  bg-gray-900 text-white">
             <nav className="w-full max-w-[1500px] h-full m-auto flex items-center justify-between px-10">
                 <div>
                     <Link href='/pages/home'>Loja</Link>
                 </div>
 
-                <ul className="w-[30%] h-full flex items-center justify-evenly text-center">
+                <ul className="w-[30%] h-full flex items-center justify-evenly text-center border-2 border-red-600">
                     <li><Link href='/pages/home'>Home</Link></li>
                     {/* <li><Link href='/pages/carrinho'>Carrinho</Link></li> */}
                     <li><Link href='/pages/contato'>Contato</Link></li>
+                    <BtnCarrinho />
                 </ul>
 
-                <BtnCarrinho />
             </nav>
         </header>
     )
